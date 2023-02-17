33458ca0d33ff2b6036c7f6b8077e0c88bb07b9e
SageOla <1olaoluwa1michaelfadeyi1@gmail.com>
2023-02-16 18:00:00 -0800

    New

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main - print a line of code using puts
- * Return: 0
+ * main - A c program that prints a line to the standard error
+ * Return: 1
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
