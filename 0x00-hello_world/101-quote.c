c0d2bbc66cd34f5ae76f83273201e03cc17df870
SageOla <1olaoluwa1michaelfadeyi1@gmail.com>
2023-02-16 18:41:02 -0800

    new

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - 8
- * Return: 1
+ * main - Task eight wahala
+ * Return: 1(success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - DoraKorpar, 2015-10-19", stdout);
-n
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
