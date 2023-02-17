cf267c5a0cf24b072bd6fdae3d1e8c212d256011
SageOla <1olaoluwa1michaelfadeyi1@gmail.com>
2023-02-16 18:29:09 -0800

    jj

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program that prints a line to the standard error
+ * main - 8
  * Return: 1
  */
 int main(void)
 {
-        fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - DoraKorpar, 2015-10-19\n", stdout);
 return (1);
 }
