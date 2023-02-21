bdcca1e1261d9fd9a11e51e85473edde68178da7
SageOla <1olaoluwa1michaelfadeyi1@gmail.com>
2023-02-20 18:20:00 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	int ones = '0';
 	int tens = '0';
-	
+
 	for (tens = '0'; tens <= '9'; tens++)/* prints tens digit*/
 	{
 		for (ones = '0'; ones <= '9'; ones++)/* prints ones digit*/
@@ -28,8 +28,6 @@ int main(void)
 			}
 		}
 	}
-	
 	putchar('\n');
-	
 	return (0);
 }
